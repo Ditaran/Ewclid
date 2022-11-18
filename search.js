86bc7788eca07f065cf6d5e0bec95e5bc186a295
ditaran <dinar.gdm@yandex.ru>
2022-11-18 23:08:35 +0500

    add burger-menu, search-form

---
+++ search.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.header__search').addEventListener('click', function() {
+        document.querySelector('.search-form').classList.toggle('.search-form__is-active')
+    })
+})
