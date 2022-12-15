a96ee8e2a0092b88621578af60b43842a3211741
ditaran <dinar.gdm@yandex.ru>
2022-12-15 13:48:59 +0500

    add links, effects, search-form, edit class names

--- search.js
+++ search.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.header__search').addEventListener('click', function() {
-        document.querySelector('.search-form').classList.toggle('.search-form__is-active')
+        document.querySelector('.search-form').classList.toggle('search-form__hidden')
     })
 })
